e4ecad27d22ac1f487a6781f5538d1be7e32184d
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-04-01 18:06:14 +0200

    eas

--- src/pages/toolbird.tsx
+++ src/pages/toolbird.tsx
@@ -6,7 +6,7 @@ import CustomerDesc from "@/components/customerdesc";
 export default function ToolBird() {
     return (
         <Layout>
-            <CaseHero heading="All the supplementary tools your tech startup needs to start, scale and run, all in one platform" description="The political landscape is accelerating in terms of complexity. To keep up with pace, Spaak Technologies works to empower organisations in digitalising operations and achieving overview in the oceans of information." />
+            <CaseHero heading="The ultimate toolbox for SaaS developers to save time and money" description="The political landscape is accelerating in terms of complexity. To keep up with pace, Spaak Technologies works to empower organisations in digitalising operations and achieving overview in the oceans of information." />
             <Reveal>
                 <img src="/spaak.png"  className="w-[94%] rounded-[14px] mx-auto h-[800px] object-cover"></img>
             </Reveal>
