36048ab282ad72d486182261efd630b072c4299b
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-04-15 22:23:13 +0200

    f

--- src/components/brands.tsx
+++ src/components/brands.tsx
@@ -2,13 +2,14 @@ import Link from "next/link"
 import { Reveal } from "./Animations/Reveal"
 
 const brandsData = [
+    {company: "ToolBird", caseDescription: "Planmates is a mobile app, made to make planning new events and plans with your friends easier", caseImage: "/products/centox.svg", tag:["web-app", "design", "frontend"], link: "/work/cases/centox"},
     {company: "Centox", caseDescription: "Planmates is a mobile app, made to make planning new events and plans with your friends easier", caseImage: "/products/centox.svg", tag:["web-app", "design", "frontend"], link: "/work/cases/centox"},
 ]
 
 export default function Brands() {
     return (
         <section className="py-[100px] pt-[0px]">
-            <div className="w-[94%] mx-auto max-w-main flex flex-col gap-14">
+            <div className="w-[94%] mx-auto max-w-main grid grid-cols-2 gap-6">
                 {brandsData.map(v => <BrandCard key={v} Obj={v} />)}
             </div>
         </section>
