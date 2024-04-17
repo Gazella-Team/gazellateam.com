0f5b647ebdb428c2bc07be8828adb36149d6274f
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-04-17 15:29:51 +0200

    jf

---
+++ src/pages/group/projects.tsx
@@ -0,0 +1,9 @@
+import Layout from "@/components/Layout";
+import GroupHero from "@/components/group/GroupHero";
+
+export default function Projects() {
+  return (
+    <Layout>
+      <GroupHero />
+    </Layout>
+)}
