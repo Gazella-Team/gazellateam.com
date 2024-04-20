a5fb2c5aa4a138f883cf07b4a337e7803de843bd
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-04-20 22:16:21 +0200

    changes

--- src/components/cases.tsx
+++ src/components/cases.tsx
@@ -1,12 +1,12 @@
 const caseData = [
-    {heading:"Minecraft Buzz Auctions"},
-    {heading:"Minecraft Buzz Auctions"},
-    {heading:"Minecraft Buzz Auctions"},
+    {heading:"Minecraft Buzz Auctions", description: "Seamless platform for slot bidding for minecraft servers", image:"/cases/spaak2.png"},
+    {heading:"Minecraft Buzz Auctions", description: "Seamless platform for slot bidding for minecraft servers", image:"/cases/spaak2.png"},
+    {heading:"Minecraft Buzz Auctions", description: "Seamless platform for slot bidding for minecraft servers", image:"/cases/spaak2.png"},
 ]
 
 export default function Cases() {
     return (
-        <section className="py-24">
+        <section className="pb-24">
             <div className="w-[94%] gap-6 mx-auto max-w-[1300px] grid md:grid-cols-1 xl:grid-cols-3">
                 {caseData.map((v, index) => <CaseCard key={v.heading} Obj={v} />)}
             </div>
@@ -16,7 +16,7 @@ export default function Cases() {
 
 function CaseCard(props:any) {
     return (
-        <div>
+        <div className="bg-[url(/cases/spaak2.png)] bg-center bg-cover min-h-[500px] rounded-xl p-10">
             <h1>{props.Obj.heading}</h1>
         </div>
     )
