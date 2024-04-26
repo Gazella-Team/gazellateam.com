7e7e88666a9816ca45111210da48c9c3b9c52f1b
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-04-26 10:40:53 +0200

    ecom

--- src/components/services.tsx
+++ src/components/services.tsx
@@ -31,6 +31,14 @@ export default function Services() {
                         </p>
                     </div>
                 </Reveal>
+                <Reveal>
+                    <div className="flex flex-col gap-4">
+                        <h2 className="text-2xl font-[300] text-main">Shopify Ecommerce Development</h2>
+                        <p className="font-[200] text-[18px] text-gray-600">
+                            What sets us apart is our commitment to providing complete product solutions for our clients. We don&apos;t just offer development services; we also provide guidance on milestones, implementation strategies, and selecting the right platforms and service providers. We understand that success goes beyond coding - it requires a comprehensive approach.
+                        </p>
+                    </div>
+                </Reveal>
             </div>
         </section>
 
