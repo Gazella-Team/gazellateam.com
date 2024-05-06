50904e2877630fbeb246258693db27427dab894b
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-05-06 15:47:21 +0200

    changes

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export default function Footer() {
     return (
-        <section className="py-[20px] bg-third text-main flex items-center border-t-[1px]">
+        <section className="py-[20px] text-main flex items-center bg-[#fafafa]">
             <div className="w-[94%] max-w-main mx-auto flex flex-col justify-between gap-[70px] text-center">
                 <p className="font-[200] text-sm text-gray-600">© {new Date().getFullYear()} Gazella Team</p>
             </div>
