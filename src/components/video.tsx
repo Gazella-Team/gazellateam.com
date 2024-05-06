409e2bb17a232a723263e75dd0d93a976a721f76
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-05-06 05:07:11 +0200

    video-sizing

--- src/components/video.tsx
+++ src/components/video.tsx
@@ -3,7 +3,7 @@ import Wistia from "./wistia";
 export default function Video() {
     return (
         <section className="py-24">
-            <div>
+            <div className="w-[90%] mx-auto max-w-[800px]">
                 <Wistia />
             </div>
         </section>
--- src/components/wistia.tsx
+++ src/components/wistia.tsx
@@ -33,13 +33,14 @@ const Wistia = () => {
   }, []);
 
   return (
-    <div className="wistia_responsive_padding" style={{ padding: '56.25% 0 0 0', position: 'relative' }}>
-      <div className="wistia_responsive_wrapper" style={{ height: '100%', left: 0, position: 'absolute', top: 0, width: '100%' }}>
-        <div className="wistia_embed wistia_async_r0o6g4p7ye seo=true videoFoam=true" style={{ height: '100%', position: 'relative', width: '100%' }}>
-          <div className="wistia_swatch" style={{ height: '100%', left: 0, opacity: 0, overflow: 'hidden', position: 'absolute', top: 0, transition: 'opacity 200ms', width: '100%' }}>
+    <div className="wistia_responsive_padding rounded-xl" style={{ padding: '56.25% 0 0 0', position: 'relative' }}>
+      <div className="wistia_responsive_wrapper rounded-xl" style={{ height: '100%', left: 0, position: 'absolute', top: 0, width: '100%' }}>
+        <div className="rounded-xl wistia_embed wistia_async_r0o6g4p7ye seo=true videoFoam=true" style={{ height: '100%', position: 'relative', width: '100%' }}>
+          <div className="wistia_swatch rounded-xl" style={{ height: '100%', left: 0, opacity: 0, overflow: 'hidden', position: 'absolute', top: 0, transition: 'opacity 200ms', width: '100%' }}>
             <img
               src="https://fast.wistia.com/embed/medias/r0o6g4p7ye/swatch"
               style={{ filter: 'blur(5px)', height: '100%', objectFit: 'contain', width: '100%' }}
+              className='rounded-xl'
               alt=""
               aria-hidden="true"
             />
