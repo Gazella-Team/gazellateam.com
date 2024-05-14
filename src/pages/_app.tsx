cacb782fb12c0766e44ee0d41999f26e9c5ece8d
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-05-14 08:44:24 +0200

    api use

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div><Script src="https://api.toolbird.io/js/script.js" strategy="lazyOnload" data-domain="usegazella.com"/> <Component {...pageProps} /></div>
 }
