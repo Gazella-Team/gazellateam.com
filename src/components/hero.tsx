5f04fad996e3d3e14add8e937f9430ba415fb3db
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-05-24 15:14:14 +0200

    test

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,13 +1,11 @@
 import Link from "next/link";
 import { Reveal } from "./Animations/Reveal";
 import TpWidget from "./TpWidget";
-import ServiceSlider from "./service-slider";
-import VideoPlayer from "./video";
 
 export default function Hero() {
     return (
         <section className="py-[60px] pb-0 bg-[url(/bgs/line.svg)] bg-cover bg-center">
-            <div className="w-[86%] mx-auto flex flex-col max-w-2xl xl:max-w-[1360px] ">
+            <div className="w-[86%] mx-auto flex flex-col max-w-4xl xl:max-w-[1360px] ">
                 <div className="flex flex-col">
                     <Reveal>
                         <div className="mx-auto max-w-5xl">
@@ -22,8 +20,8 @@ export default function Hero() {
                     </Reveal>
                     <Reveal>
                         <div className="flex mx-auto max-w-5xl justify-end items-center gap-4 mb-[60px]">
-                            <Link className="bg-main text-white rounded-full px-7 py-2 text-sm font-[300] border-2 border-main" href={"/contact"}>Contact us</Link>
-                            <Link className="bg-white text-main rounded-full px-7 py-2 text-sm font-[300] border-2 border-main hover:bg-main hover:text-white transition-all" href={"/services"}>Learn more</Link>
+                            <Link className="bg-main text-white rounded-full px-7 py-2 text-xs md:text-sm font-[300] border-2 border-main" href={"/contact"}>Contact us</Link>
+                            <Link className="bg-white text-main rounded-full px-7 py-2 text-xs md:text-sm font-[300] border-2 border-main hover:bg-main hover:text-white transition-all" href={"/services"}>Learn more</Link>
                         </div>
                     </Reveal>
                 </div>
