436ecd744b75d9e82d9d2462e9a29e754b86f136
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-06-14 11:10:03 +0200

    nmew

--- src/components/cases.tsx
+++ src/components/cases.tsx
@@ -3,14 +3,15 @@ import { Reveal } from "./Animations/Reveal"
 import { url } from "inspector"
 
 const caseData = [
-    {heading:"Minecraft Buzz Auctions", description: "Seamless platform for slot bidding for minecraft servers", image:"/cases/spaak.png", link:"/minecraft-buzz-auctions"},
+    {heading:"coad.dk", description: "Seamless platform for slot bidding for minecraft servers", image:"/cases/spaak.png", link:"/minecraft-buzz-auctions"},
     {heading:"Minecraft Buzz Auctions", description: "Seamless platform for slot bidding for minecraft servers", image:"/cases/spaak2.png", link:"/minecraft-buzz-auctions"},
+    {heading:"coad.dk", description: "Seamless platform for slot bidding for minecraft servers", image:"/cases/spaak.png", link:"/minecraft-buzz-auctions"},
 ]
 
 export default function Cases() {
     return (
         <section>
-            <div className="w-[94%] pb-24 gap-6 mx-auto grid grid-cols-3 max-w-[1300px]">
+            <div className="w-[94%] pb-24 gap-6 mx-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 max-w-[1300px]">
                 {caseData.map((v, index) => <CaseCard key={v.heading} Obj={v} />)}
             </div>
         </section>
@@ -20,7 +21,10 @@ export default function Cases() {
 function CaseCard(props:any) {
     return (
         <Link href={props.Obj.link}>
-            <div className="bg-center bg-[#fafafa] bg-cover min-h-[500px] rounded-xl text-white flex flex-col justify-end">
+            <div className="bg-center bg-[#fafafa] bg-cover min-h-[500px] rounded-xl text-white flex flex-col justify-between">
+                <div className="p-8">
+                    <p className="font-[system-ui] text-sm text-main">.web</p>
+                </div>
                 <div className="text-main p-10 rounded-xl">
                     <h1 className="text-md mb-1">{props.Obj.heading}</h1>
                     <p className="font-[200]">Vi er et ungt og nørdet team</p>
