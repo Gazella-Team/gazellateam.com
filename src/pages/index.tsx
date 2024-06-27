f051ad0ed39d5e93496470a508ef73291d2f92f0
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-06-27 09:19:57 +0200

    new

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Layout from "@/components/Layout";
+import Meta from "@/components/Layout/Meta";
 import Hero from "@/components/hero";
 import PageIntro from "@/components/reused/PageIntro";
 import VideoPlayer from "@/components/video";
@@ -6,6 +7,7 @@ import VideoPlayer from "@/components/video";
 export default function Home() {
   return (
     <Layout>
+     <Meta title={`Home - Gazella Team`} />
       <PageIntro heading="We boost businesses with seamless software and user-experience" description="We help companies of all sizes sell more, build workflows and launch innovative ideas through tailormade software development." />
       <div className="w-[94%] rounded-2xl mb-20 mx-auto max-w-[1300px]">
         <VideoPlayer />
