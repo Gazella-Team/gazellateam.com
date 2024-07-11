0d9ac22625bf5445422a09839cc1a04177d26764
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-07-11 14:00:26 +0200

    toolbird

--- src/components/cases.tsx
+++ src/components/cases.tsx
@@ -3,8 +3,8 @@ import { Reveal } from "./Animations/Reveal"
 import { url } from "inspector"
 
 const caseDataFirst = [
-    {heading:"coad.dk", description: "Seamless platform for slot bidding for minecraft servers", image:"/cases/test.webp", link:"/minecraft-buzz-auctions"},
-    {heading:"Minecraft Auctions", description: "Seamless platform for slot bidding for minecraft servers", image:"/cases/spaak2.png", link:"/minecraft-buzz-auctions"},
+    {heading:"Toolbird", description: "Seamless platform for slot bidding for minecraft servers", image:"/cases/test.webp", link:"/minecraft-buzz-auctions"},
+    {heading:"Minecraft Auctions", description: "Seamless platform for slot bidding for minecraft servers", image:"/cases/toolbird.webp", link:"/minecraft-buzz-auctions"},
     {heading:"coad.dk", description: "Seamless platform for slot bidding for minecraft servers", image:"/cases/spaak.png", link:"/minecraft-buzz-auctions"},
     {heading:"coad.dk", description: "Seamless platform for slot bidding for minecraft servers", image:"/cases/test.webp", link:"/minecraft-buzz-auctions"},
     {heading:"Minecraft Auctions", description: "Seamless platform for slot bidding for minecraft servers", image:"/cases/spaak2.png", link:"/minecraft-buzz-auctions"},
@@ -16,7 +16,7 @@ const caseDataFirst = [
 
 const caseDataSecond = [
     {heading:"coad.dk", description: "Seamless platform for slot bidding for minecraft servers", image:"/cases/test.webp", link:"/minecraft-buzz-auctions"},
-    {heading:"Minecraft Auctions", description: "Seamless platform for slot bidding for minecraft servers", image:"/cases/spaak2.png", link:"/minecraft-buzz-auctions"},
+    {heading:"Minecraft Auctions", description: "Seamless platform for slot bidding for minecraft servers", image:"/cases/toolbird.webp", link:"/minecraft-buzz-auctions"},
     {heading:"coad.dk", description: "Seamless platform for slot bidding for minecraft servers", image:"/cases/spaak.png", link:"/minecraft-buzz-auctions"},
     {heading:"coad.dk", description: "Seamless platform for slot bidding for minecraft servers", image:"/cases/test.webp", link:"/minecraft-buzz-auctions"},
     {heading:"Minecraft Auctions", description: "Seamless platform for slot bidding for minecraft servers", image:"/cases/spaak2.png", link:"/minecraft-buzz-auctions"},
@@ -30,10 +30,10 @@ export default function Cases() {
     return (
         <section>
             <div className="w-[94%] pb-24 gap-10 mx-auto flex justify-between max-w-[900px]">
-                <div className="flex flex-col gap-10">
+                <div className="flex flex-col gap-20">
                     {caseDataFirst.map((v, index) => <CaseCard Obj={v} />)}
                 </div>
-                <div className="flex flex-col gap-10 mt-40">
+                <div className="flex flex-col gap-20 mt-40">
                     {caseDataSecond.map((v, index) => <CaseCard Obj={v} />)}
                 </div>
             </div>
@@ -45,7 +45,7 @@ function CaseCard(props:any) {
     return (
         <Link href={props.Obj.link}>
             <div className="flex flex-col gap-8">
-                <img className="min-h-[560px]" src="/cases/test.webp"></img>
+                <img className="min-h-[560px]" src="/cases/toolbird.webp"></img>
                 <div>
                     <h2 className="text-2xl">{props.Obj.heading}</h2>
                 </div>
