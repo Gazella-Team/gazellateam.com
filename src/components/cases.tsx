d18a067fcf1f1dc43edc0a3a8077fcc387e808c4
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-07-14 15:03:56 +0200

    new

--- src/components/cases.tsx
+++ src/components/cases.tsx
@@ -3,24 +3,17 @@ import { Reveal } from "./Animations/Reveal"
 import { url } from "inspector"
 
 const caseDataFirst = [
-    {heading:"Toolbird", domain:"toolbird.io", description: "Seamless platform for slot bidding for minecraft servers", image:"/cases/test.webp", link:"/minecraft-buzz-auctions"},
+    {heading:"Toolbird", domain:"toolbird.io", description: "Seamless platform for slot bidding for minecraft servers", image:"/blog-images/how-much-does-an-app-cost/main.webp", link:"/minecraft-buzz-auctions"},
     {heading:"Minecraft Auctions", description: "Seamless platform for slot bidding for minecraft servers", image:"/cases/toolbird.webp", link:"/minecraft-buzz-auctions"},
 ]
 
-const caseDataSecond = [
-    {heading:"coad.dk", description: "Seamless platform for slot bidding for minecraft servers", image:"/cases/toolbird.webp", link:"/minecraft-buzz-auctions"},
-    {heading:"Minecraft Auctions", description: "Seamless platform for slot bidding for minecraft servers", image:"/cases/test.webp", link:"/minecraft-buzz-auctions"},
-]
 
 export default function Cases() {
     return (
         <section>
-            <div className="w-[94%] pb-24 gap-16 mx-auto flex justify-between max-w-[1000px]">
-                <div className="flex flex-col gap-20">
+            <div className="w-[94%] max-w-[1300px] pb-24 gap-16 mx-auto">
+                <div className="flex flex-col gap-20 grid grid-cols-2">
                     {caseDataFirst.map((v, index) => <CaseCard Obj={v} />)}
-                </div>
-                <div className="flex flex-col gap-20 mt-40">
-                    {caseDataSecond.map((v, index) => <CaseCard Obj={v} />)}
                 </div>
             </div>
         </section>
@@ -31,10 +24,9 @@ function CaseCard(props:any) {
     return (
         <Link href={props.Obj.link}>
             <div className="flex flex-col gap-8">
-                <img className="min-h-[560px]" src={props.Obj.image}></img>
-                <div className="flex flex-col gap-2">
-                    <p className="text-gray-400 paragraph font-[200] text-xl">toolbird.io</p>
-                    <h2 className="text-3xl">{props.Obj.heading}</h2>
+                <img className="h-[560px] object-cover" src={props.Obj.image}></img>
+                <div className="absolute m-8 bg-gray-200 rounded-full p-1 px-5">
+                    <h2 className="text-lg paragraph">{props.Obj.heading}</h2>
                 </div>
             </div>
         </Link>
--- src/pages/cases.tsx
+++ src/pages/cases.tsx
@@ -7,7 +7,7 @@ import PageIntro from "@/components/reused/PageIntro";
 export default function Solutions() {
     return (
         <Layout>
-            <PageIntro starter="madewithlove" smallWidth={true} heading="Cases" description="" />
+            <PageIntro starter="madewithlove" heading="Cases" description="Dive into our various cases and internal projects that we develop to inspire and improve businesses." />
             <CaseComponent />
         </Layout>
     )
