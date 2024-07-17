c29ef51b7c815e630a83654ff6f6dfb73b8a82cf
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-07-17 13:34:05 +0200

    new

--- src/pages/group/index.tsx
+++
@@ -1,9 +0,0 @@
-import Layout from "@/components/group/Layout";
-import Hero from "@/components/group/hero";
-
-export default function Home() {
-  return (
-    <Layout>
-      <Hero />
-    </Layout>
-)}
