c9d06a3b9d2d2bf79c4801ba4b4a5cd41989e31a
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-08-15 13:54:00 +0200

    sd

--- src/pages/cases.tsx
+++ src/pages/cases.tsx
@@ -1,20 +1,72 @@
+import { Reveal } from '@/components/Animations/Reveal'
 import Layout from '@/components/Layout'
-import SecondHero from '@/components/reused/DefaultHero'
-import CaseComponent from '@/components/cases'
-import Team from '@/components/team'
+import Meta from '@/components/Layout/Meta'
 import PageIntro from '@/components/reused/PageIntro'
-import MarginPageIntro from '@/components/reused/MarginPageIntro'
+import ServicesComponent from '@/components/services'
+import { ArrowRight } from 'lucide-react'
+import Link from 'next/link'
 
-export default function Solutions() {
+export default function Cases() {
 	return (
-		<Layout transparentNav={true}>
-			<MarginPageIntro
-				headingSize="sm"
-				starter="madewithlove"
-				heading="Mere end 10.000 samlede udviklingstimer i baggagen"
-				description="Dyk ned i vores forskellige cases og interne projekter, som vi udvikler for at inspirere og forbedre virksomheder."
-			/>
-			<CaseComponent />
+		<Layout cta={true}>
+			<Meta keywords='app, iOS, Android, cross-platform, udvikling, mobil' description='Fremtiden er mobil. Vi specialiserer os i cross-platform app-udvikling for at levere unikke mobiloplevelser til både iOS og Anroid. Gennem hele udviklingsprocessen sørger vi for kontinuerlig iteration og feedback, efterfulgt af post-lancering support for at sikre en fremtidssikret skalerbar løsning, der passer til din vision.' title={`Gazella Team: Vi er specialister i udvikling af apps til iOS og Android.`} />
+			<section className="py-16 md:py-24 bg-cover bg-center flex flex-col items-end">
+				<div className="w-[90%] mx-auto max-w-[700px] flex flex-col">
+					<div className="max-w-2xl">
+						<Reveal>
+							<div className="mb-14 flex flex-col gap-10 md:gap-12">
+								<h2 className="text-4xl md:text-5xl xl:text-5xl leading-[120%] md:leading-[120%] xl:leading-[120%] font-semibold text-main">
+									Mere end <span className='text-second'>10.000</span> dedikerede udviklingstimer i baggagen
+								</h2>
+							</div>
+						</Reveal>
+					</div>
+					<div className="flex flex-col mt-5 gap-6 sm:gap-10">
+						<Link target="_blank" href={"https://cal.com/lasseosmann/discoverycall"} className="flex items-center gap-5">
+							<div className="p-4 border rounded-full hover:bg-third hover:border-third hover:text-white transition-all text-main border-main">
+								<ArrowRight strokeWidth={1.5} />
+							</div>
+							<p className="text-2xl">www.coad.dk</p>
+						</Link>
+						<Link target="_blank" href={"https://cal.com/lasseosmann/discoverycall"} className="flex items-center gap-5">
+							<div className="p-4 border rounded-full hover:bg-third hover:border-third hover:text-white transition-all text-main border-main">
+								<ArrowRight strokeWidth={1.5} />
+							</div>
+							<p className="text-2xl">www.coad.dk</p>
+						</Link>
+						<Link target="_blank" href={"https://cal.com/lasseosmann/discoverycall"} className="flex items-center gap-5">
+							<div className="p-4 border rounded-full hover:bg-third hover:border-third hover:text-white transition-all text-main border-main">
+								<ArrowRight strokeWidth={1.5} />
+							</div>
+							<p className="text-2xl">www.coad.dk</p>
+						</Link>
+						<Link target="_blank" href={"https://cal.com/lasseosmann/discoverycall"} className="flex items-center gap-5">
+							<div className="p-4 border rounded-full hover:bg-third hover:border-third hover:text-white transition-all text-main border-main">
+								<ArrowRight strokeWidth={1.5} />
+							</div>
+							<p className="text-2xl">www.coad.dk</p>
+						</Link>
+						<Link target="_blank" href={"https://cal.com/lasseosmann/discoverycall"} className="flex items-center gap-5">
+							<div className="p-4 border rounded-full hover:bg-third hover:border-third hover:text-white transition-all text-main border-main">
+								<ArrowRight strokeWidth={1.5} />
+							</div>
+							<p className="text-2xl">www.coad.dk</p>
+						</Link>
+						<Link target="_blank" href={"https://cal.com/lasseosmann/discoverycall"} className="flex items-center gap-5">
+							<div className="p-4 border rounded-full hover:bg-third hover:border-third hover:text-white transition-all text-main border-main">
+								<ArrowRight strokeWidth={1.5} />
+							</div>
+							<p className="text-2xl">www.coad.dk</p>
+						</Link>
+						<Link target="_blank" href={"https://cal.com/lasseosmann/discoverycall"} className="flex items-center gap-5">
+							<div className="p-4 border rounded-full hover:bg-third hover:border-third hover:text-white transition-all text-main border-main">
+								<ArrowRight strokeWidth={1.5} />
+							</div>
+							<p className="text-2xl">www.coad.dk</p>
+						</Link>
+					</div>
+				</div>
+			</section>
 		</Layout>
 	)
 }
